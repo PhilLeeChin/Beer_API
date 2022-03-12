d25f3e74c43f5eb2309f909e83745e90c5ba072c
Philip <phil.leechin@outlook.com>
2022-03-12 17:55:21 -0500

    add seed data for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+philip = User.create(
+    username: 'Naos',
+    email: 'test@email.com',
+    password: '1234'
+)
+
+beverly = User.create(
+    username: 'Spoilie',
+    email: 'test@email.com',
+    password: '5678'
+)
+
+beers = Beer.create([
+    {
+        name: "Corona",
+        description: "Best beer, but got more sales due to COVID19.",
+        alcohol_level: 4.6,
+        country_name: "USA",
+        year_created: 1925,
+        user_id: philip.id
+    },
+
+    {
+        name: "Moscato",
+        description: "A great wine to use when hosting dinners.",
+        alcohol_level: 5,
+        country_name: "Italy",
+        year_created: 1870,
+        user_id: beverly.id
+    }
+])
+
+Style.create([
+    {
+        served: "Cold with lemon or lime wedge",
+        appetizers: "Chicken wings with sweet glaze, or tacos",
+        beer_id: beers.first.id,
+        user_id: philip.id
+    },
+
+    {
+        served: "Chilled with a few ice cubes",
+        appetizers: "Pies, Cobblers, or Vanilla Ice-cream",
+        beer_id: beers.second.id,
+        user_id: beverly.id
+    }
+])
