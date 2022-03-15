9cae5c95ce5bb2395b5b92b18a37ede9a55ac882
Philip <phil.leechin@outlook.com>
2022-03-15 19:52:59 -0400

    generate user resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,34 +22,34 @@ beers = Beer.create([
     {
         name: "Corona",
         description: "Best beer, but got more sales due to COVID19.",
-        alcohol_level: 4.6,
-        country_name: "USA",
-        year_created: 1925,
+        alcohol_lvl: 4.6,
+        country: "USA",
+        yr_made: 1925,
         user_id: philip.id
     },
 
     {
         name: "Moscato",
         description: "A great wine to use when hosting dinners.",
-        alcohol_level: 5,
-        country_name: "Italy",
-        year_created: 1870,
+        alcohol_lvl: 5,
+        country: "Italy",
+        yr_made: 1870,
         user_id: beverly.id
     }
 ])
 
-Style.create([
-    {
-        served: "Cold with lemon or lime wedge",
-        appetizers: "Chicken wings with sweet glaze, or tacos",
-        beer_id: beers.first.id,
-        user_id: philip.id
-    },
+# Style.create([
+#     {
+#         served: "Cold with lemon or lime wedge",
+#         appetizers: "Chicken wings with sweet glaze, or tacos",
+#         beer_id: beers.first.id,
+#         user_id: philip.id
+#     },
 
-    {
-        served: "Chilled with a few ice cubes",
-        appetizers: "Pies, Cobblers, or Vanilla Ice-cream",
-        beer_id: beers.second.id,
-        user_id: beverly.id
-    }
-])
+#     {
+#         served: "Chilled with a few ice cubes",
+#         appetizers: "Pies, Cobblers, or Vanilla Ice-cream",
+#         beer_id: beers.second.id,
+#         user_id: beverly.id
+#     }
+# ])
