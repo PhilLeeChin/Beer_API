40daf596e557f51e5f0b36b09eeedae64bedc7cb
Philip <phil.leechin@outlook.com>
2022-03-16 18:52:04 -0400

    add get route for autologin

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :beers
 
   resources :sessions, only: [:create]
+
+  get '/me', to: "sessions#autologin"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
